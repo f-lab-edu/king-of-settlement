7f4a5d705eef91567686ef5b3228136df75fbc09
melody-story <rnlqls7454@naver.com>
2024-02-23 13:46:52 +0900

    feature: 스프링부트 초기세팅 추가

---
+++ src/main/java/son/king_of_settlement/KingOfSettlementApplication.java
@@ -0,0 +1,13 @@
+package son.king_of_settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingOfSettlementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingOfSettlementApplication.class, args);
+	}
+
+}
