ea23182f12b5dffb1d0d1c69c4167a8ec3970990
melody-story <rnlqls7454@naver.com>
2024-03-29 23:23:26 +0900

    [#6] test: user 생성 테스트 코드 추가

--- src/test/java/son/kingofsettlement/user/UserTest.java
+++ src/test/java/son/kingofsettlement/user/UserTest.java
@@ -5,29 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import jakarta.persistence.EntityManager;
-import son.kingofsettlement.user.domain.User;
+import son.kingofsettlement.user.exception.entity.User;
 import son.kingofsettlement.user.repository.UserRepository;
-import son.kingofsettlement.user.service.UserService;
 
 @SpringBootTest
 class UserTest {
-
-	@Autowired
-	EntityManager em;
-
-	@Autowired
-	UserService userService;
 
 	@Autowired
 	UserRepository userRepository;
 
 	@Test
 	void save() {
-		User user1 = new User("Melody");
-		User member1 = userService.join(user1);
-		System.out.println(member1);
+		// given
+		User user1 = new User("myeonghee.son@gmail.com");
+		// when
+		User member1 = userRepository.save(user1);
+		// then
 		Assertions.assertEquals(user1, member1);
-		// return member1.getId();
 	}
 }
