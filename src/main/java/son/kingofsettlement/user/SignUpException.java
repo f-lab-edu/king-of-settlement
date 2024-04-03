7e3fc6fe9ef3d6723fc8d1eb2094a75df8016744
melody-story <rnlqls7454@naver.com>
2024-04-03 18:24:49 +0900

    [#6] feature: 회원가입 예외 추가

---
+++ src/main/java/son/kingofsettlement/user/SignUpException.java
@@ -0,0 +1,12 @@
+package son.kingofsettlement.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class SignUpException extends RuntimeException {
+
+	public SignUpException(String message) {
+		super(message);
+	}
+}
