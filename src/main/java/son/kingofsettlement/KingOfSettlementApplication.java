055d15e70ef4f3c3c8939a16e4b57c2ab7c3f6ce
melody-story <rnlqls7454@naver.com>
2024-04-06 17:54:41 +0900

    [#6] fix: 회원가입 코드리뷰반영 - @SpringBootApplication 주석 수정

--- src/main/java/son/kingofsettlement/KingOfSettlementApplication.java
+++ src/main/java/son/kingofsettlement/KingOfSettlementApplication.java
@@ -3,8 +3,12 @@ package son.kingofsettlement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	@SpringBootApplication
+	: Spring Boot 애플리케이션의 주 클래스에 붙이며, 구성(Configuration), 자동 구성(Auto-configuration),
+	컴포넌트 스캔을 단순하게 처리해준다.	이를 통해 애플리케이션의 설정과 시작이 편리해짐.
+*/
 @SpringBootApplication
-// Spring Boot 애플리케이션의 주 클래스에 붙이며, 구성(Configuration), 자동 구성(Auto-configuration), 컴포넌트 스캔을 단순하게 처리해준다. 이를 통해 애플리케이션의 설정과 시작을 편리해짐.
 public class KingOfSettlementApplication {
 
 	public static void main(String[] args) {
