f8712a6a61a3d033ae8a9778c721993469b7b1f5
melody-story <rnlqls7454@naver.com>
2024-04-10 02:53:47 +0900

    [#6] comment: 어노테이션 주석 추가

--- src/main/java/son/kingofsettlement/user/entity/User.java
+++ src/main/java/son/kingofsettlement/user/entity/User.java
@@ -7,17 +7,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import son.kingofsettlement.user.dto.UserStatus;
 
+// JPA 엔티티 클래스임을 나타내는 어노테이션으로, 데이터베이스의 테이블과 매핑되는 클래스임을 표시
 @Entity
+// Lombok 라이브러리에서 제공하는 어노테이션으로, 클래스의 필드에 대한 getter 메소드를 자동으로 생성
 @Getter
-@Table
 public class User {
-
+	// JPA 엔티티 클래스에서 주(primary) 식별자 필드를 지정하는 어노테이션으로, 해당 필드가 엔티티의 주키임을 나타냄
 	@Id
+	// 주(primary) 키 값이 자동으로 생성되는 방식을 설정하는 어노테이션으로, 주로 자동으로 증가하는 식별자를 생성할 때 사용
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// 엔티티 클래스의 필드와 데이터베이스 테이블의 컬럼을 매핑하는데 사용되며,  name, nullable, unique 등의 속성을 가지고 있어, 데이터베이스 컬럼의 속성을 정의
 	@Column(name = "user_id")
 	private Long id;
 	private String email;
--- src/main/java/son/kingofsettlement/user/service/SignUpService.java
+++ src/main/java/son/kingofsettlement/user/service/SignUpService.java
@@ -9,8 +9,11 @@ import son.kingofsettlement.user.entity.User;
 import son.kingofsettlement.user.exception.SignUpException;
 import son.kingofsettlement.user.repository.UserRepository;
 
+// 해당 클래스가 비즈니스 로직을 수행하는 서비스 클래스임을 나타내는 어노테이션으로, 주로 서비스 계층의 클래스에 사용
 @Service
+// Lombok 라이브러리에서 제공하는 어노테이션으로, final 필드가 있는 생성자를 생성해주는 역할
 @RequiredArgsConstructor
+// 트랜잭션 처리를 지원하기 위한 어노테이션으로, 해당 메소드나 클래스에 트랜잭션을 적용
 @Transactional(readOnly = true)
 public class SignUpService {
 
