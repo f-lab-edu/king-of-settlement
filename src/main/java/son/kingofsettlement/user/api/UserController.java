47a46a249d9db394b3ad83e552b7e8c5c19f76a9
melody-story <rnlqls7454@naver.com>
2024-04-10 05:08:42 +0900

    [#6] fix: 회원가입 코드리뷰반영 - signUpRequest: userValidator 삭제 및 Validation 추가 적용

--- src/main/java/son/kingofsettlement/user/api/UserController.java
+++ src/main/java/son/kingofsettlement/user/api/UserController.java
@@ -1,7 +1,5 @@
 package son.kingofsettlement.user.api;
 
-import static son.kingofsettlement.user.exception.UserValidator.*;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import son.kingofsettlement.user.dto.SignUpRequest;
 import son.kingofsettlement.user.dto.SignUpResponse;
@@ -40,13 +37,11 @@ public class UserController {
 		// 객체의 유효성을 검사하고 유효하지 않은 경우에는 예외를 발생.
 		@Valid SignUpRequest req, BindingResult bindingResult) {
 		try {
-			if (bindingResult.hasErrors()) {
+			if (bindingResult.hasFieldErrors()) {
 				throw new SignUpException(bindingResult.getFieldError().getDefaultMessage());
 			}
-			validatePassword(req.getPassword());
-			validateNickname(req.getNickname());
 			signUpService.signUp(req);
-		} catch (ValidationException | SignUpException e) {
+		} catch (SignUpException e) {
 			return ResponseEntity.badRequest().body(new SignUpResponse(e.getMessage()));
 		}
 		return ResponseEntity.status(HttpStatus.CREATED).body(new SignUpResponse("SignUp Succeed"));
