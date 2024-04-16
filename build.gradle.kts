374ffb8146022a85f76f2836e7f6438f89891eab
melody-story <rnlqls7454@naver.com>
2024-04-16 15:31:44 +0900

    [#3] chore: mysql 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     compileOnly("org.projectlombok:lombok")
-    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j:8.3.0")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.springframework.boot:spring-boot-starter-mail")
 }
 
 tasks.withType<Test> {
