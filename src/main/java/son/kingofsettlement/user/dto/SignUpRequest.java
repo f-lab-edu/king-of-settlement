66806762c2e3ed71c59d4bc06df57b7fb56f4a57
melody-story <rnlqls7454@naver.com>
2024-04-21 20:37:11 +0900

    [#7] feature : 회원가입 기능 - validation null, blank 추가

--- src/main/java/son/kingofsettlement/user/dto/SignUpRequest.java
+++ src/main/java/son/kingofsettlement/user/dto/SignUpRequest.java
@@ -1,6 +1,8 @@
 package son.kingofsettlement.user.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,8 +10,15 @@ import lombok.ToString;
 @Getter
 @ToString
 public class SignUpRequest {
-	@Email(message = "이메일 형식이 잘못되었습니다.") String email;
-	@Pattern(regexp = "^(?=.*[A-Z]).(?=.*[a-z]).(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{|}~]).(?=.*[0-9]).{8,15}$", message = "비밀번호를 확인해주세요") String password;
+	@NotNull
+	@NotBlank
+	@Email(message = "이메일 형식이 잘못되었습니다.")
+	String email;
+
+	@NotNull
+	@NotBlank
+	@Pattern(regexp = "^(?=.*[A-Z]).(?=.*[a-z]).(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{|}~]).(?=.*[0-9]).{8,15}$", message = "비밀번호를 확인해주세요")
+	String password;
 
 	public SignUpRequest() {
 	}
