b66ef4a5bbab138081dd9c6cb08b1ca526205a65
melody-story <rnlqls7454@naver.com>
2024-04-22 18:21:57 +0900

    [#7] feature : 회원가입 기능 - Optional 적용

--- src/main/java/son/kingofsettlement/user/repository/UserRepository.java
+++ src/main/java/son/kingofsettlement/user/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package son.kingofsettlement.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import son.kingofsettlement.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	User findOneByEmail(String email);
+	Optional<User> findOneByEmail(String email);
 }
