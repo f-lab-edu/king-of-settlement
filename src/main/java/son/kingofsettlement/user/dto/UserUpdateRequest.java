1225b7b719980a1811a0cab6093396a8bef2781d
melody-story <rnlqls7454@naver.com>
2024-04-22 18:22:19 +0900

    [#24] feature: 유저수정 개발중

---
+++ src/main/java/son/kingofsettlement/user/dto/UserUpdateRequest.java
@@ -0,0 +1,32 @@
+package son.kingofsettlement.user.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UserUpdateRequest {
+
+	@NotNull
+	String nickname;
+	@NotNull
+	String profileUrl;
+	@NotNull
+	String introduction;
+
+	public UserUpdateRequest() {
+	}
+
+	public UserUpdateRequest(String nickname, String profileUrl, String introduction) {
+		this.nickname = nickname;
+		this.profileUrl = profileUrl;
+		this.introduction = introduction;
+	}
+}
+
+
+
+
+
+
