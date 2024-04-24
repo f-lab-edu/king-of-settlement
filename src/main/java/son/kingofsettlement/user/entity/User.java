18cbf599703ff234445444eb0ad6218fb17d617a
melody-story <rnlqls7454@naver.com>
2024-04-24 12:31:30 +0900

    [#8] feature : 로그인 기능(세션)2 - repository : findOneBySessionId 추가 - service : 트랜젝션 분리, 유저객체 리턴 - user, repository : 세션아이디 업데이트 코드 수정중

--- src/main/java/son/kingofsettlement/user/entity/User.java
+++ src/main/java/son/kingofsettlement/user/entity/User.java
@@ -1,17 +1,10 @@
 package son.kingofsettlement.user.entity;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import son.kingofsettlement.user.dto.UserStatus;
+
+import java.time.LocalDateTime;
 
 // JPA 엔티티 클래스임을 나타내는 어노테이션으로, 데이터베이스의 테이블과 매핑되는 클래스임을 표시
 @Entity
@@ -49,6 +42,11 @@ public class User {
 		this.password = password;
 		this.activityStatus = UserStatus.INACTIVE;
 		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	public void updateSessionId(String sessionId) {
+		this.sessionId = sessionId;
 		this.updatedAt = LocalDateTime.now();
 	}
 }
