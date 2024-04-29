9671395db97df61e5f68a28b58cb0b8945375837
melody-story <rnlqls7454@naver.com>
2024-04-29 15:53:52 +0900

    프론트 수정

--- src/main/java/son/kingofsettlement/web/WebController.java
+++ src/main/java/son/kingofsettlement/web/WebController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/web")
 public class WebController {
@@ -20,12 +23,20 @@ public class WebController {
 	}
 
 	@GetMapping("/home")
-	public String home(Model model) {
-		return "home";
+	public String home(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			return "home";
+		}
+		return "login";
 	}
 
 	@GetMapping("/profile")
-	public String profile(Model model) {
-		return "profile";
+	public String profile(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			return "profile";
+		}
+		return "login";
 	}
 }
