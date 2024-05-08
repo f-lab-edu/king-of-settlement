1d4ca8041cfd6cfe99e5844f719e418478335faa
melody-story <rnlqls7454@naver.com>
2024-05-08 10:56:28 +0900

    [#6] fix: 회원가입 코드리뷰반영 - exception 에 지정된 https status 제거(controller 에서 제어)

--- src/main/java/son/kingofsettlement/user/exception/SignUpException.java
+++ src/main/java/son/kingofsettlement/user/exception/SignUpException.java
@@ -1,10 +1,5 @@
 package son.kingofsettlement.user.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-// 예외가 발생했을 때 지정한 HTTP 응답 코드를 반환
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class SignUpException extends RuntimeException {
 	public SignUpException(String message) {
 		super(message);
