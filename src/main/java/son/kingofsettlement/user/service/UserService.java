35d15fa6b9d4ae9aeaf72e57278c786fe748ded1
melody-story <rnlqls7454@naver.com>
2024-05-09 17:08:36 +0900

    [#7] fix : 회원가입 코드리뷰 반영 - 비밀번호 암호화 방법1 SHA-256 + salt로 변경 적용

--- src/main/java/son/kingofsettlement/user/service/UserService.java
+++ src/main/java/son/kingofsettlement/user/service/UserService.java
@@ -1,8 +1,6 @@
 package son.kingofsettlement.user.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import son.kingofsettlement.user.dto.SignUpRequest;
@@ -11,6 +9,12 @@ import son.kingofsettlement.user.exception.EncryptException;
 import son.kingofsettlement.user.exception.SignUpException;
 import son.kingofsettlement.user.repository.UserRepository;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+
 // 해당 클래스가 비즈니스 로직을 수행하는 서비스 클래스임을 나타내는 어노테이션으로, 주로 서비스 계층의 클래스에 사용
 @Service
 // Lombok 라이브러리에서 제공하는 어노테이션으로, final 필드가 있는 생성자를 생성해주는 역할
@@ -18,21 +22,44 @@ import son.kingofsettlement.user.repository.UserRepository;
 // 트랜잭션 처리를 지원하기 위한 어노테이션으로, 해당 메소드나 클래스에 트랜잭션을 적용
 public class UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	@Transactional
-	public User signUp(SignUpRequest req) throws EncryptException, SignUpException {
-		isDuplicatedUser(req.getEmail());
-		PasswordEncoder passwordEncoder = Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-		String encryptedEmail = AESEncryption.encrypt(req.getEmail());
-		String hashedPassword = passwordEncoder.encode(req.getPassword());
-		User user = User.inActiveStatusOf(encryptedEmail, hashedPassword);
-		return userRepository.save(user);
-	}
+    @Transactional
+    public User signUp(SignUpRequest req) throws EncryptException, SignUpException {
+        isDuplicatedUser(req.getEmail());
+        String encryptedEmail = AESEncryption.encrypt(req.getEmail());
+        byte[] salt = generateSalt();
+        String encryptedPassword = hashPassword(req.getPassword(), salt);
+        User user = User.inActiveStatusOf(encryptedEmail, encryptedPassword);
+        return userRepository.save(user);
+    }
 
-	public void isDuplicatedUser(String email) throws SignUpException {
-		if (userRepository.findOneByEmail(email) != null) {
-			throw new SignUpException("중복된 이메일입니다.");
-		}
-	}
+    private String hashPassword(String password, byte[] salt) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.reset();
+            digest.update(salt);
+            StringBuilder encryptPassword = new StringBuilder();
+            byte[] hashedByteArrayPassword = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            for (byte b : hashedByteArrayPassword) {
+                encryptPassword.append(String.format("%02x", b));
+            }
+            return String.valueOf(encryptPassword);
+        } catch (NoSuchAlgorithmException e) {
+            throw new EncryptException(e.getMessage());
+        }
+    }
+
+    private byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[256];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    public void isDuplicatedUser(String email) throws SignUpException {
+        if (userRepository.findOneByEmail(email).isPresent()) {
+            throw new SignUpException("중복된 이메일입니다.");
+        }
+    }
 }
