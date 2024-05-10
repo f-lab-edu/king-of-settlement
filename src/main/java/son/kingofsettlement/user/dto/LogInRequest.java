c3f2adc658183094bb4356573ed3aa6c80f4883a
melody-story <rnlqls7454@naver.com>
2024-05-10 17:41:01 +0900

    [#8] fix : 로그인 코드리뷰 반영 - 불필요 생성자 삭제

--- src/main/java/son/kingofsettlement/user/dto/LogInRequest.java
+++ src/main/java/son/kingofsettlement/user/dto/LogInRequest.java
@@ -8,14 +8,6 @@ import lombok.ToString;
 @Getter
 @ToString
 public class LogInRequest {
-	@Email(message = "이메일 형식이 잘못되었습니다.") String email;
-	@Pattern(regexp = "^(?=.*[A-Z]).(?=.*[a-z]).(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{|}~]).(?=.*[0-9]).{8,15}$", message = "비밀번호를 확인해주세요") String password;
-
-	public LogInRequest() {
-	}
-
-	public LogInRequest(String email, String password) {
-		this.email = email;
-		this.password = password;
-	}
+    @Email(message = "이메일 형식이 잘못되었습니다.") String email;
+    @Pattern(regexp = "^(?=.*[A-Z]).(?=.*[a-z]).(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{|}~]).(?=.*[0-9]).{8,15}$", message = "비밀번호를 확인해주세요") String password;
 }
