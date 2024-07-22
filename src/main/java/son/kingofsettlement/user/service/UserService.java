dcd7e6e0743a31035459038505ee6b0ea0a3e14e
melody-story <rnlqls7454@naver.com>
2024-07-22 23:31:45 +0900

    [#7] fix: 포매팅 적용

--- src/main/java/son/kingofsettlement/user/service/UserService.java
+++ src/main/java/son/kingofsettlement/user/service/UserService.java
@@ -1,13 +1,13 @@
 package son.kingofsettlement.user.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 import son.kingofsettlement.user.dto.SignUpRequest;
 import son.kingofsettlement.user.entity.User;
 import son.kingofsettlement.user.exception.SignUpException;
 import son.kingofsettlement.user.repository.UserRepository;
-
 
 // 해당 클래스가 비즈니스 로직을 수행하는 서비스 클래스임을 나타내는 어노테이션으로, 주로 서비스 계층의 클래스에 사용
 @Service
@@ -16,17 +16,17 @@ import son.kingofsettlement.user.repository.UserRepository;
 // 트랜잭션 처리를 지원하기 위한 어노테이션으로, 해당 메소드나 클래스에 트랜잭션을 적용
 public class UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    @Transactional
-    public User signUp(SignUpRequest req) throws SignUpException {
-        String encryptedEmail = AESEncryption.encrypt(req.getEmail());
-        if (userRepository.findOneByEmail(encryptedEmail).isPresent()) {
-            throw new SignUpException("중복된 이메일입니다.");
-        }
-        String salt = BCrypt.gensalt(10);
-        String encryptedPassword = BCrypt.hashpw(req.getPassword(), salt);
-        User user = User.of(encryptedEmail, encryptedPassword);
-        return userRepository.save(user);
-    }
+	@Transactional
+	public User signUp(SignUpRequest req) throws SignUpException {
+		String encryptedEmail = AESEncryption.encrypt(req.getEmail());
+		if (userRepository.findOneByEmail(encryptedEmail).isPresent()) {
+			throw new SignUpException("중복된 이메일입니다.");
+		}
+		String salt = BCrypt.gensalt(10);
+		String encryptedPassword = BCrypt.hashpw(req.getPassword(), salt);
+		User user = User.of(encryptedEmail, encryptedPassword);
+		return userRepository.save(user);
+	}
 }
